1159c8ed8b36502d44c4c9234f7ecad324e876ae
D Ionut <dionut711@gmail.com>
2021-09-12 22:48:34 +0300

    Replace ice wall placeholder

--- Assets/Scripts/Abilities/IceWall.cs
+++ Assets/Scripts/Abilities/IceWall.cs
@@ -2,29 +2,9 @@ using UnityEngine;
 
 public class IceWall : ElementalWall
 {
-    public GameObject softWallVisual;
-    public GameObject hardWallVisual;
-
-    [Header("Read only")]
-    [SerializeField] protected bool _isHard = false;
-
-    public bool IsHard
-    {
-        get => _isHard;
-        set
-        {
-            _isHard = value;
-
-            // change visual
-            softWallVisual.SetActive(!_isHard);
-            hardWallVisual.SetActive(_isHard);
-        }
-    }
-
     protected override void Start()
     {
         base.Start();
         ElementalType = ElementalType.Ice;
-        IsHard = _isHard;
     }
 }
