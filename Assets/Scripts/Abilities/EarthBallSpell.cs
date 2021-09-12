eed3c4b05f86dbd58726f9e45d3d8e11aaf927ab
D Ionut <dionut711@gmail.com>
2021-09-12 15:36:03 +0300

    Scripts for elemental spells

---
+++ Assets/Scripts/Abilities/EarthBallSpell.cs
@@ -0,0 +1,8 @@
+using System;
+
+[Serializable]
+public class EarthBallSpell : ElementalBallSpell
+{
+    public EarthBallSpell() : base(ElementalType.Earth)
+    { }
+}
---
+++ Assets/Scripts/Abilities/WindBallSpell.cs
@@ -0,0 +1,8 @@
+using System;
+
+[Serializable]
+public class WindBallSpell : ElementalBallSpell
+{
+    public WindBallSpell() : base(ElementalType.Wind)
+    { }
+}
